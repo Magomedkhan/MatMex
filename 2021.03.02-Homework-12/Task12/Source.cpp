db67d0ebf7a089d4f369330b932774dc7f301019
Magomedkhan <st087937@student.spbu.ru>
2021-03-03 17:08:23 +0300

    st

--- 2021.03.02-Homework-12/Task12/Source.cpp
+++ 2021.03.02-Homework-12/Task12/Source.cpp
@@ -41,45 +41,48 @@ void calc(T1 a, T2 b, T1(*op)(T1, T2))
 template<class T1, class T2>
 void resolveOpNumber(T1 operand1, T2 operand2, char sign)
 {
+	T1(*ops[5])(T1, T2) = { sum, diff, mult, div, rem };
+
 	switch (sign)
 	{
 	case '+':
 	{
-		calc(operand1, operand2, sum);
+		calc(operand1, operand2, ops[0]);
 		break;
 	}
 	case '-':
 	{
-		calc(operand1, operand2, diff);
+		calc(operand1, operand2, ops[1]);
 		break;
 	}
 	case '*':
 	{
-		calc(operand1, operand2, mult);
+		calc(operand1, operand2, ops[2]);
 		break;
 	}
 	case '/':
 	{
-		calc(operand1, operand2, div);
+		calc(operand1, operand2, ops[3]);
 		break;
 	}
 	case '%':
 	{
-		calc(operand1, operand2, rem);
+		calc(operand1, operand2, ops[4]);
 		break;
 	}
 	}
 }
 
-
-
-int main()
+template<class T1, class T2>
+void input(T1 operand1, T2 operand2)
 {
 	char sign;
-	int operand1, operand2; // как задать шаблон?
 	cin >> operand1 >> operand2 >> sign;
 	resolveOpNumber(operand1, operand2, sign);
+}
 
-
+int main(int argc, char** argv)
+{
+	input(0, 0);
 	return 0;
 }

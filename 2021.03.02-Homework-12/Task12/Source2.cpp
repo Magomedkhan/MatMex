33a2fbb7145a4c3e63f30f27400fdc06a32a0315
Magomedkhan <st087937@student.spbu.ru>
2021-03-07 03:33:42 +0300

    st

--- 2021.03.02-Homework-12/Task12/Source2.cpp
+++ 2021.03.02-Homework-12/Task12/Source2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 template <class T1, class T2>
@@ -20,7 +21,7 @@ T1 mult(T1 a, T2 b)
 }
 
 template <class T1, class T2>
-T1 div(T1 a, T2 b)
+T1 div_(T1 a, T2 b)
 {
 	return a / b;
 }
@@ -29,7 +30,19 @@ T1 div(T1 a, T2 b)
 template <class T1, class T2>
 T1 rem(T1 a, T2 b)
 {
-	return a % b;
+	int t_a = (int)(a * 10);  int t_b = (int)(b * 10);
+
+	if (t_a % 10 == 0 && t_b % 10 == 0)
+	{
+		int t = (int)a / b;
+		t = a - t * b;
+		return t;
+
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 template<class T1, class T2>
@@ -41,9 +54,9 @@ void calc(T1 a, T2 b, T1(*op)(T1, T2))
 template<class T1, class T2>
 void resolveOpNumber(T1 operand1, T2 operand2, char sign)
 {
-	T1(*ops[5])(T1, T2) = { sum, diff, mult, div, rem };
+	T1(*ops[5])(T1, T2) = { sum, diff, mult, div_, rem };
 
-	switch (sign)
+	switch(sign)
 	{
 	case '+':
 	{
@@ -73,16 +86,20 @@ void resolveOpNumber(T1 operand1, T2 operand2, char sign)
 	}
 }
 
-template<class T1, class T2>
-void input(T1 operand1, T2 operand2)
-{
-	char sign;
-	cin >> operand1 >> operand2 >> sign;
-	resolveOpNumber(operand1, operand2, sign);
-}
 
-int main(int argc, char** argv)
+
+
+int main(int argc, char* argv[])
 {
-	input(0, 0);
-	return 0;
+	if (argv[0] == NULL || argv[1] == NULL || argv[2] == NULL || argv[3] == NULL || argv[4] == NULL || argv[5] == NULL || argv[6] == NULL || argv[7] == NULL) { cout << "ban"; return 0; }
+	else {
+		double a = stod(argv[3]); double b = stod(argv[5]);
+		char* sign0 = argv[7];
+		char sign = *sign0;
+		if (rem(a, b) != -1) {
+			cout << a << " " << sign << " " << b << " " << "=" << " "; resolveOpNumber(a, b, sign);
+		}
+		else { cout << "ban"; }
+		return 0;
+	}
 }
